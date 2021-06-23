27c4e1e5cd56d9bbba6c313ddf58478b2b52d201
EmiyaKiritsugu3 <61369082+EmiyaKiritsugu3@users.noreply.github.com>
2021-06-23 17:30:43 +0000

    Add "while"

--- main.c
+++ main.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 
 int main(void){
+  
   int option;
-  printf("Generic Project to Prove C Language Programing Skill\nDeveloped by @inamarjunior - Junho, 2021\nChoose a option entering the corresponding number:\n1 - ???\n2 - ???\n3 - Quit\n");
-  scanf("%d", &option);
-  switch(option){
-    case 1:
-      printf("Still in development, come back later\n");
-      break;
-    case 2:
-      printf("Still in development, come back later\n");
-      break;
-    case 3:
-      break;
+  printf("Generic Project to Prove C Language Programing Skill\nDeveloped by @inamarjunior - Junho, 2021");
+  do{  
+    printf("\nChoose a option entering the corresponding number:\n1 - ???\n2 - ???\n3 - Quit\n");
+    scanf("%d", &option);
+    switch(option){
+      case 1:
+        printf("Still in development, come back later\n");
+        
+        break;
+      case 2:
+        printf("Still in development, come back later\n");
+        break;
+      case 3:
+        break;
+    }
 
   }
+  while(option != 3);
 }
