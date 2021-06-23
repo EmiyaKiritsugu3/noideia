3b08109f4b037eff4d28c38040a062bb1c47ed32
EmiyaKiritsugu3 <61369082+EmiyaKiritsugu3@users.noreply.github.com>
2021-06-23 17:12:51 +0000

    Something

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main(void){
+  int option;
+  printf("Generic Project to Prove C Language Programing Skill\nDeveloped by @inamarjunior - Junho, 2021\nChoose a option entering the corresponding number:\n1 - ???\n2 - ???\n3 - Quit\n");
+  scanf("%d", &option);
+  switch(option){
+    case 1:
+      printf("Still in development, come back later\n");
+      break;
+    case 2:
+      printf("Still in development, come back later\n");
+      break;
+    case 3:
+      break;
+
+  }
 }
